eba27b24caedbd2b30939c792ab28931f733dd11
unknown <kainesv86@gmail.com>
2021-12-14 23:50:58 +0700

    Fix - ENV_NODE

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,35 @@ import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './auth/user.entity';
+import { ConfigModule } from '@nestjs/config';
+
+const Config = ConfigModule.forRoot({
+  isGlobal: true,
+  envFilePath: `/config/.env.${process.env.NODE_ENV}`,
+});
 
 const DBConfig = TypeOrmModule.forRoot({
   type: 'mysql',
-  host: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  username: 'root',
+  password: '1234567890Aa',
+  database: 'task_management',
   synchronize: true,
   keepConnectionAlive: true,
   entities: [User],
 });
 
 @Module({
-  imports: [TasksModule, AuthModule],
+  imports: [
+    // --- Config
+    Config,
+    DBConfig,
+    // -- Module
+    TasksModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
