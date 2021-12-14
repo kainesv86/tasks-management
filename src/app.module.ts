56732396ebd43c7d666332b101076d010e415fe2
unknown <kainesv86@gmail.com>
2021-12-14 22:20:10 +0700

    Add - ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './auth/user.entity';
+
+const DBConfig = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  keepConnectionAlive: true,
+  entities: [User],
+});
 
 @Module({
   imports: [TasksModule, AuthModule],
